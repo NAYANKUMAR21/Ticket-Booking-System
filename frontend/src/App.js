ef074bc3c344e44d37867c91c3704dc7e34013b8
Nayan Kumar <naayaankumar@gmail.com>
2023-07-11 15:57:29 +0530

    initial commit

---
+++ frontend/src/App.js
@@ -0,0 +1,15 @@
+import logo from './logo.svg';
+import './App.css';
+import { Text } from '@chakra-ui/react';
+
+function App() {
+  return (
+    <div className="App">
+      <Text fontSize={'xl'} textAlign={'center'}>
+        Hello world
+      </Text>
+    </div>
+  );
+}
+
+export default App;
